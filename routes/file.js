ffa4e6efecdaf663ee9292b531413c8f2f7352a9
Fanch <francois.gerbes@sb-roscoff.fr>
2019-07-10 09:26:15 +0200

    use callback with render in nunjucks

--- routes/file.js
+++ routes/file.js
@@ -11,21 +11,19 @@ var fs = require('fs');
 nunjucks.configure('templates', { autoescape: true });
 
 
-
 module.exports = {
-  // Todo: find if we need a callback
-  create_ssh_config: function (user, callback) {
-    var filepath = user.home + "/.ssh";
-    var filename = "config.test";
-    logger.info("Create file", filepath + "/" + filename);
-    var content = nunjucks.render('ssh_config', { user: user });
-    // Todo: find if we need to do the write in the render callback
-    fs.mkdirSync(filepath, { recursive: true }, function (err) {
-      fs.chmod(filepath, 0o700);
-      fs.writeFile(filepath + "/" + filename, content, function (err) {
-        fs.chmod(filepath + "/" + filename, 0o600);
-      });
-    });
-  }
-
+    // Todo: find if we need a callback
+    create_ssh_config: function (user, callback) {
+        var filepath = user.home + "/.ssh";
+        var filename = "config.test";
+        logger.info("Create file", filepath + "/" + filename);
+        nunjucks.render('ssh_config', { user: user }, function (err, content) {
+            fs.mkdirSync(filepath, { recursive: true }, function (err) {
+                fs.chmod(filepath, 0o700);
+                fs.writeFile(filepath + "/" + filename, content, function (err) {
+                    fs.chmod(filepath + "/" + filename, 0o600);
+                });
+            });
+        });
+    }
 }
